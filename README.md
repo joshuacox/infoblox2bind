1ffb98b30684b2e10c3218b6370f6d4f0b2d03d2
Josh Cox <josh@webhosting.coop>
2022-06-28 19:05:24 -0500

    Initial Import

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
 # infoblox2bind
 convert infoblox 2 bind zone
+
+
+### Usage
+
+You might want to export some variables before running.
+
+Set the input file:
+
+```
+export INPUT_CSV_FILE='/tmp/555_All DNS Objects.csv'
+```
+
+Set the ZONE:
+
+```
+export ZONE=example.org
+```
+
+Set the IP of this name server:
+
+```
+export THIS_SERVER_IP=10.0.0.1}
+```
+
+```
+export SERIAL=202206281738}
+export REFRESH=604800}
+export RETRY=86400}
+export TTL=604800}
+export EXPIRE=2419200}
+export NEGATIVE_CACHE_TTL=604800}
+```
+
+Then run the script:
+
+```
+./infoblox2bind.bash
+using /tmp/555_All DNS Objects.csv
+
+
+Your zone file is here --> /tmp/tmp.Ee2B2lSOG0/zone.example.org.db
+```
+
+### Debug
+
+Get some extra verbosity:
+
+```
+export VERBOSE=11
+```
---
+++ infoblox2bind.bash
@@ -0,0 +1,86 @@
+#!/bin/bash
+# grep -P '^arecord' 220627_All\ DNS\ Objects.csv |cut -f2,4 -d, --output-delimiter=' '|sed 's/\.jhdc\.local//' |awk '{print "export IP="$1" ; export HOST="$2" ; echo -e $HOST\tIN\tA\t$IP "}' |bash |tr ' ' '\t'|tail -n5}'
+: ${INPUT_CSV_FILE:=input.csv}
+: ${ZONE:=example.com}
+: ${SERIAL:=202206281738}
+: ${REFRESH:=604800}
+: ${RETRY:=86400}
+: ${TTL:=604800}
+: ${EXPIRE:=2419200}
+: ${NEGATIVE_CACHE_TTL:=604800}
+: ${THIS_SERVER_IP:=10.0.0.1}
+
+if [[ -f $INPUT_CSV_FILE ]]; then
+  echo "using $INPUT_CSV_FILE"
+else
+  echo 'Please supply an INPUT_CSV_FILE'
+  echo 'e.g.'
+  echo "export INPUT_CSV_FILE=/tmp/this.csv; $0"
+  exit 1
+fi
+
+TMP=$(mktemp -d)
+
+cat <<EOF > $TMP/zone.$ZONE.db
+; BIND data file for us-ne-1 lan0
+;
+\$TTL   $TTL
+@       IN      SOA     $ZONE. (
+                     admin.$ZONE. ; Owner
+                   $SERIAL         ; Serial - increment after save
+                         $REFRESH         ; Refresh
+                          $RETRY         ; Retry
+                        $EXPIRE         ; Expire
+                         $NEGATIVE_CACHE_TTL )       ; Negative Cache TTL
+;
+
+@           IN      NS    $ZONE.
+@           IN      A     $THIS_SERVER_IP ; Address of this server
+EOF
+
+echo_slurp () {
+  if [[ $VERBOSE -gt 10 ]]; then
+    echo $slurped|tr ' ' '\n'|less
+  fi
+}
+
+find_records_by_type () {
+  TYPE=$1
+  slurped=$( grep -P  "^$TYPE" "$INPUT_CSV_FILE" | cut -d "," -f2,4 | sed "s/\.$ZONE//" )
+  echo_slurp
+  while IFS="," read -r IP HOST 
+  do
+    printf '%s\tIN\tA\t%s\n' $HOST $IP >> $TMP/$TYPE.zone.$ZONE.db
+  done <<< $slurped
+}
+
+find_records_by_type_host_first () {
+  TYPE=$1
+  slurped=$( grep -P  "^$TYPE" "$INPUT_CSV_FILE" | cut -d "," -f2,4 | sed "s/\.$ZONE//" )
+  echo_slurp
+  while IFS="," read -r HOST IP  
+  do
+    printf '%s\tIN\tA\t%s\n' $HOST $IP >> $TMP/$TYPE.zone.$ZONE.db
+  done <<< $slurped
+  #done < <( grep -P  "$TYPE" "$INPUT_CSV_FILE" | cut -d "," -f2,4 | sed "s/\.$ZONE//" ) >> $TMP/zone.$ZONE.db
+}
+
+find_records_by_type 'arecord'
+
+# not certain what the difference is here
+find_records_by_type 'hostaddress'
+# I think this is redundant or may be used to make PTR records, but am including as it does add some unique hosts
+find_records_by_type_host_first 'hostrecord'
+
+cat $TMP/arecord.zone.$ZONE.db  $TMP/hostaddress.zone.$ZONE.db  $TMP/hostrecord.zone.$ZONE.db |sort|uniq >> $TMP/zone.$ZONE.db
+#cat $TMP/arecord.zone.$ZONE.db  |sort|uniq >> $TMP/zone.$ZONE.db
+
+if [[ $VERBOSE -gt 0 ]]; then
+  #head -n25 $TMP/zone.$ZONE.db
+  #less $TMP/zone.$ZONE.db
+  sort $TMP/zone.$ZONE.db |uniq -c|sort -rn|less
+  wc -l $TMP/zone.$ZONE.db
+  #ls -alh $TMP|less
+  #cat $TMP/arecord.zone.$ZONE.db  $TMP/hostaddress.zone.$ZONE.db  $TMP/hostrecord.zone.$ZONE.db |sort|uniq -c|sort -rn|less
+fi
+echo -e "\n\nYour zone file is here --> $TMP/zone.$ZONE.db"
